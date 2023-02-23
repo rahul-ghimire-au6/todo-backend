7d003dd84df71029d8fa176d86984b1fe77638b8
Rahul R Ghimire <ghimirerahul@gmail.com>
2023-02-23 22:03:09 +0530

    done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
     cors({
         origin:"*",  
-        methods: "GET,POST,PUT,DELETE",
+        methods: "GET,POST,PUT,DELETE,PATCH",
         preflightContinue: false,
         optionsSuccessStatus: 204,
         credentials: true
